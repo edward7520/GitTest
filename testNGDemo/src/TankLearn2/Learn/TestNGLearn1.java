c96358e66fd3e35c0cb893ab3fe146c8fc6dcedb
edward7520 <xuhuawei@outlook.com>
2015-06-17 10:16:48 +0800

    testNG代码的第一次提交

---
+++ testNGDemo/src/TankLearn2/Learn/TestNGLearn1.java
@@ -0,0 +1,27 @@
+package TankLearn2.Learn;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+
+public class TestNGLearn1 {
+
+    @BeforeClass
+    public void beforeClass() {
+        System.out.println("this is before class");
+    }
+
+    @Test
+    public void TestNgLearn() {
+        System.out.println("this is TestNG test case");
+    }
+
+    @AfterClass
+    public void afterClass() {
+        System.out.println("this is after class");
+    }
+}
